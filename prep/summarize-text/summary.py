78f685265bb4f97bcf608e48abd94e167474e2c9
Rohan Gulati <rohangulati94@gmail.com>
2026-05-16 18:17:55 +0000

    feat: (sumarize-text) support text file for text input and update summarization logic

--- prep/summarize-text/summary.py
+++ prep/summarize-text/summary.py
@@ -1,12 +1,31 @@
+import os
+
 from openai import OpenAI
 
 client = OpenAI()
 
 print("Hello world!!")
+print("Current directory:", os.getcwd())
+with open("./prep/summarize-text/input.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+# response = client.responses.create(
+#     model="gpt-5-mini",
+#     input="Summarize: Python is a programming language."
+# )
 
 response = client.responses.create(
     model="gpt-5-mini",
-    input="Summarize: Python is a programming language."
-)
+    input=f"""
+    You are a helpful summarization assistant.
 
+    Summarize the following text:
+    - Keep it concise
+    - Use bullet points
+    - Focus on key ideas
+
+    TEXT:
+    {text}
+    """
+)
 print(response.output_text)
